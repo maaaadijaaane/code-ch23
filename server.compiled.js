255a1ac5c2822816fe4f4c50f7de684a87bd241d
mholcomb <madison.holcomb@wsu.edu>
2020-10-28 15:37:14 -0700

    Commit 2: Class Assignment 10/28 getting project set up for remote

--- server.compiled.js
+++ server.compiled.js
@@ -41,7 +41,7 @@ var app = (0, _express["default"])(); //////////////////////////////////////////
 //using the mongoose library.
 //////////////////////////////////////////////////////////////////////////
 
-var connectStr = 'mongodb://localhost:27017/appdb';
+var connectStr = 'mongodb+srv://dbAdmin:zthgycH5U48wfSS@speedgolf.xz3vv.mongodb.net/appdb?retryWrites=true&w=majority';
 
 _mongoose["default"].connect(connectStr, {
   useNewUrlParser: true,
